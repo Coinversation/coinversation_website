7a7b5ce11c44f0567f2a4968985dd2a2a5fd0452
hailiting <hailiting@yeah.net>
2021-12-07 19:01:30 +0800

    debug

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-// import { PageLoading } from "components/PageLoading";
+import { PageLoading } from "components/PageLoading";
 import { BrowserRouter } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
 import "@/i18n";
@@ -9,24 +9,24 @@ import "./flexible";
 import "./App.less";
 
 const listen = () => {
-  // if (document.readyState === "complete") {
-  ReactDOM.render(
-    <BrowserRouter>
-      <Suspense fallback={<div></div>}>
-        <App />
-      </Suspense>
-    </BrowserRouter>,
-    document.getElementById("root")
-  );
-  // } else {
-  //   // assert is loading
-  //   ReactDOM.render(
-  //     <React.StrictMode>
-  //       <PageLoading />
-  //     </React.StrictMode>,
-  //     document.getElementById("root")
-  //   );
-  // }
+  if (document.readyState === "complete") {
+    ReactDOM.render(
+      <BrowserRouter>
+        <Suspense fallback={<div></div>}>
+          <App />
+        </Suspense>
+      </BrowserRouter>,
+      document.getElementById("root")
+    );
+  } else {
+    // assert is loading
+    ReactDOM.render(
+      <React.StrictMode>
+        <PageLoading />
+      </React.StrictMode>,
+      document.getElementById("root")
+    );
+  }
 };
 
 document.onreadystatechange = listen;
