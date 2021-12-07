3675fb6ea8a6150272f7c5d5150885aab3522111
hailiting <hailiting@yeah.net>
2021-12-07 18:50:40 +0800

    debug

--- src/page/joinUs/components/bannerComponents/joinBannerFr.tsx
+++ src/page/joinUs/components/bannerComponents/joinBannerFr.tsx
@@ -1,11 +1,10 @@
-import React from "react";
-// import React, { useContext } from "react";
-// import Identicon from "@polkadot/react-identicon";
+import React, { useContext } from "react";
+import Identicon from "@polkadot/react-identicon";
 import "./joinBannerFr.less";
-// import { ParachainData } from "../../context";
-// import { sortName, getGrandPrizePool } from "../../utils/utils";
+import { ParachainData } from "../../context";
+import { sortName, getGrandPrizePool } from "../../utils/utils";
 const BannerFr = () => {
-  // const parachainData = useContext(ParachainData);
+  const parachainData = useContext(ParachainData);
   return (
     <div className="join_bannerFr">
       <a
@@ -16,7 +15,7 @@ const BannerFr = () => {
       >
         Reward rules&gt;&gt;
       </a>
-      {/* <h2>
+      <h2>
         {getGrandPrizePool(parachainData?.count)[1]}
         <i>CTO</i>
       </h2>
@@ -54,7 +53,7 @@ const BannerFr = () => {
               );
             })
           : null}
-      </ul> */}
+      </ul>
     </div>
   );
 };
