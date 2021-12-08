f1b938eff20ddea52ff60e2b3bfa0f306a7ba49c
hailiting <hailiting@yeah.net>
2021-12-08 09:28:24 +0800

    debug 01

--- src/page/joinUs/joinIndex.tsx
+++ src/page/joinUs/joinIndex.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import HeaderWidget from "@/components/header/headerWidget";
 import FooterWidget from "@/components/footer/footerWidget";
-import Height from "@/components/height/heightWidget";
-import JoinBanner from "./components/joinBanner";
-import Process from "./components/process";
-import ListOfWinners from "./components/listOfWinners";
+// import Height from "@/components/height/heightWidget";
+// import JoinBanner from "./components/joinBanner";
+// import Process from "./components/process";
+// import ListOfWinners from "./components/listOfWinners";
 import "./joinIndex.less";
 // import { jest } from "@types/jest";
 import {
@@ -52,13 +52,14 @@ export default function JoinIndex() {
                   <ParachainData.Provider value={parachainData}>
                     <ParachainDataSet.Provider value={setParachainData}>
                       <div className="joinIndex_inner_inner">
-                        <Height height={60} />
+                        {/* <Height height={60} />
                         <JoinBanner />
                         <Height height={40} />
                         <Process />
                         <Height height={40} />
                         <ListOfWinners />
-                        <Height height={60} />
+                        <Height height={60} /> */}
+                        <h1>ssss</h1>
                       </div>
                     </ParachainDataSet.Provider>
                   </ParachainData.Provider>
